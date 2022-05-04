ab68e18fe7374964958c7387c681bc87c976740d
Andreas Kurka <andreas.kurka@adesso.de>
2022-05-04 13:12:24 +0200

    Added (not implemented) a component for login

---
+++ Angular-Cheat-Sheet.md
@@ -0,0 +1,4 @@
+# Adding new components
+* Right-click on the Frontend-project in Visual Studio, choose Open in Terminal
+* In Terminal type: ng generate component xyz
+
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ALoginComponent } from './a-login/a-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ALoginComponent
   ],
   imports: [
     BrowserModule
