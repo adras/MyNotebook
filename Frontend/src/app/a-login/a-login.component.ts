0363d8715fac00c6bf6c0535be880cebe0d33a5b
Andreas Kurka <andreas.kurka@adesso.de>
2022-05-04 14:00:26 +0200

    Added binding for password field

--- Frontend/src/app/a-login/a-login.component.ts
+++ Frontend/src/app/a-login/a-login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./a-login.component.css']
 })
 export class ALoginComponent implements OnInit {
+  public password: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onLogin(): void {
+
+    alert(`Your password is: '${this.password}'. Now everone can read it ;) `);
+  }
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ALoginComponent } from './a-login/a-login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { ALoginComponent } from './a-login/a-login.component';
     ALoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // Required for [(ngModel)] bindings
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
