0ed3759d4568c441cea1d2b8f48f53da5106560c
Andreas Kurka <andreas.kurka@adesso.de>
2022-05-04 15:09:08 +0200

    Added LoginResponse model Added SHA-password encryption Added Login request

---
+++ Frontend/src/app/a-login/LoginResponse.ts
@@ -0,0 +1,5 @@
+export class LoginResponse {
+   isLoggedIn: boolean = false;
+   message: string = '';
+   result: boolean = false;
+}
--- Frontend/src/app/a-login/a-login.component.ts
+++ Frontend/src/app/a-login/a-login.component.ts
@@ -1,37 +1,61 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+import * as sha256 from 'crypto-js/sha256';
+import { Observable } from 'rxjs';
+import { LoginResponse } from './LoginResponse'
 
 @Component({
   selector: 'app-a-login',
   templateUrl: './a-login.component.html',
   styleUrls: ['./a-login.component.css']
 })
+
+
 export class ALoginComponent implements OnInit {
   public password: string = "";
-  private http: HttpClient;
+  //private http: HttpClient;
 
-  constructor(http: HttpClient) {
-    this.http = http;
+  constructor(private http: HttpClient) {
+    //this.http = http;
   }
 
   ngOnInit(): void {
   }
 
   onLogin(): void {
-    var url = "http://yourserver.com/notes/index.php";
+    // Note: Using SHA256 to hash passwords on the client side seems to be deprecated
+    // See: https://stackoverflow.com/a/43903139/7671671
+    // TODO: Research alternatives
+    // Since it's not planned to update the backend right now, sha256 needs to be used
 
-    var body = {
-      action: "login",
-      password : "test"
-    }
+    var url = "http://yourserver.com/notes/index.php";
+    var shaPwd = sha256(this.password).toString();
+
+    //SHA256.fuu;
+    //var body = {
+    //  action: "login",
+    //  password: shaPwd
+    //}
+    const params = new HttpParams()
+      .set("action", "login")
+      .set("password", shaPwd)
 
     // POST
     // action: login
     // password: hash
+    const headers = { 'content-type': 'application/json' };
 
-    
-
-    alert(`Your password is: '${this.password}'. Now everone can read it ;) `);
-    //this.http.get(url, options);
+    let response = this.http.get<LoginResponse>(url, { params });
+    response.subscribe({
+      next(loginResponse) {
+        console.log('IsLoggedIn: ', loginResponse.isLoggedIn);
+        console.log('message: ', loginResponse.message);
+        console.log('result: ', loginResponse.result);
+      },
+      error(msg) {
+        console.log('Error Getting Location: ', msg);
+      }
+    });
   }
 }
