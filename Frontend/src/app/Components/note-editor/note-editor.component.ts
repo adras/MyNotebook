8a4e234db2283605cda39c2f839c6d682a853863
Andreas Kurka <andreas.kurka@gmail.com>
2022-05-08 21:24:14 +0200

    Enabled editor - no save support yet

--- Frontend/src/app/Components/note-editor/note-editor.component.ts
+++ Frontend/src/app/Components/note-editor/note-editor.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Editor } from 'ngx-editor';
 
 @Component({
   selector: 'app-note-editor',
   templateUrl: './note-editor.component.html',
   styleUrls: ['./note-editor.component.css']
 })
-export class NoteEditorComponent implements OnInit {
-
-  constructor() { }
+export class NoteEditorComponent implements OnInit, OnDestroy {
+  @Input() html: string = '';
+  editor!: Editor;
+  //html: string = '';
 
   ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  // make sure to destory the editor
+  ngOnDestroy(): void {
+    this.editor!.destroy();
   }
 
 }
