b8da08e78757c953241a57315d2649440b6cd210
Andreas Kurka <andreas.kurka@adesso.de>
2022-05-18 12:19:13 +0200

    Started implementation for multiple tags in input box - currently overwritten by autocomplete

--- Frontend/src/app/Components/tag-suggest/tag-suggest.component.ts
+++ Frontend/src/app/Components/tag-suggest/tag-suggest.component.ts
@@ -25,14 +25,18 @@ export class TagSuggestComponent implements OnInit {
     );
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
+  private _filter(tagsInput: string): string[] {
+    const filterValue = tagsInput.toLowerCase();
 
-    const result = this.allTags.filter(tag => tag.name.toLowerCase().includes(filterValue)).map(tag => tag.name);
-    const result2 = Object.assign([], result);
+    // Split the current text in the input box by spaces to get a list of all tags currently entered
+    const tags = tagsInput.split(' ');
 
-    const result3 = ['allnotes', 'new', 'test', 'SvenRules', 'neu', 'empty'];
-    return result2;
+    // Find the last tag in the list to use it for autocomplete
+    const currentTag = tags[tags.length - 1];
+
+    const result = this.allTags.filter(tag => tag.name.toLowerCase().includes(currentTag)).map(tag => tag.name);
+
+    return result;
   }
 
 }
