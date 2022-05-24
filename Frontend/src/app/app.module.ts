cf2da7dd282988473f54fc1c228bcd7cade219c3
Andreas Kurka <andreas.kurka@gmail.com>
2022-05-24 19:47:03 +0200

    Added fxlayout. Some buttons at top now floating ploperly

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -60,7 +61,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatChipsModule,
     MatIconModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule 
   ],
 
   providers: [],
